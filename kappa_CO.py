b363106180086194d120ca53c4458d29b0cd69aa
Christopher Rowe <chris.rowe19@outlook.com>
2022-02-05 02:19:44 +0000

    Attempts to calculate kappa_CO.

---
+++ kappa_CO.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from DataAccess import constants, load_catalouge_field, ParticleReadConversion_EagleSnapshot, particles_by_group, Simulations, SimulationModels, ParticleType, UnitSystem
+
+from assembily_history import physical_centeral_mass_positions, assembily_history
+from data_over_time import produce_simulations_graph
+
+relitive_data_root = ".\\gm_for_mphys"
+
+def func(halo, subhalo, tag, simulation):
+    galaxy_centre = physical_centeral_mass_positions[simulation][tag]#TODO: check whether cgs or physical code is uncommented!!!
+    if galaxy_centre is None:
+        raise LookupError("No data avalible.")
+
+    r_200 = load_catalouge_field("Group_R_Crit200", "FOF", tag, simulation, SimulationModels.RECAL, relitive_data_root, UnitSystem.cgs)[assembily_history[simulation][tag]["halo"] - 1]
+    #selection_radius = r_200
+    selection_radius = 9.257*(10**22)# 30 KPc
+
+    snapshot = ParticleReadConversion_EagleSnapshot(tag, simulation, SimulationModels.RECAL, relitive_data_root)
+
+    # Calculate the net angular momentum (axis vector)
+    particle_types = (ParticleType.gas, ParticleType.star, ParticleType.black_hole)
+    net_angular_momentum = np.array([0, 0, 0], dtype = np.float64)
+    for j in range(len(particle_types)):
+        _, particle_locations_box_adjusted = snapshot.particle_read_sphere(particle_types[j], "Coordinates", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
+        particle_velocities = snapshot.particle_read_sphere(particle_types[j], "Velocity", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs)
+        particle_velocities[:, 0] -= particle_velocities[:, 0].mean()
+        particle_velocities[:, 1] -= particle_velocities[:, 1].mean()
+        particle_velocities[:, 2] -= particle_velocities[:, 2].mean()
+        particle_masses = snapshot.particle_read_sphere(particle_types[j], "Mass", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs)
+
+        net_angular_momentum += np.sum(np.cross(particle_locations_box_adjusted, particle_velocities) * particle_masses[:, None], axis = 0)
+
+    # Make a unit vector
+    rotation_axis_vector = net_angular_momentum / np.linalg.norm(net_angular_momentum, 2)
+
+    # Calculate angular momenta and kinetic energy for all stars
+    _, star_particle_locations_box_adjusted = snapshot.particle_read_sphere(ParticleType.star, "Coordinates", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
+    star_particle_velocities = snapshot.particle_read_sphere(ParticleType.star, "Velocity", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs)
+    star_particle_velocities[:, 0] -= star_particle_velocities[:, 0].mean()
+    star_particle_velocities[:, 1] -= star_particle_velocities[:, 1].mean()
+    star_particle_velocities[:, 2] -= star_particle_velocities[:, 2].mean()
+    star_particle_masses = snapshot.particle_read_sphere(ParticleType.star, "Mass", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs)
+
+    angular_momenta = np.cross(star_particle_locations_box_adjusted, star_particle_velocities) * star_particle_masses[:, None]
+    kinetic_energies = 0.5 * star_particle_masses * (star_particle_velocities**2).sum(axis = 1)
+    
+    kappa_CO = kinetic_energies[angular_momenta.dot(rotation_axis_vector) > 0].sum() / kinetic_energies.sum()
+
+    return kappa_CO
+
+produce_simulations_graph(func, "$\kappa_{CO}$", "Co-Rotating Stellar Kinetic Energy Fraction", log_x = True)
+
--- oriented_final_snapshot_images.py
+++ oriented_final_snapshot_images.py
@@ -6,7 +6,6 @@ from DataAccess import ParticleReadConversion_EagleSnapshot, load_catalouge_fiel
 from assembily_history import physical_centeral_mass_positions, assembily_history
 from coordinate_transform import produce_disk_x_basis, transform_coordinates
 
-sim = Simulations.Organic
 model = SimulationModels.RECAL
 tag = "028_z000p000"
 
@@ -14,47 +13,24 @@ relitive_data_root = ".\\gm_for_mphys"
 
 galaxy_centres = [physical_centeral_mass_positions[sim][constants.tags[-1]] for sim in Simulations]#TODO: check whether cgs or physical code is uncommented!!!
 
-galaxy_picture_edge_offsets__global = [0.15, 0.025, 0.025]# physical units
-
 simulations = (Simulations.Early, Simulations.Organic, Simulations.Late)
 for i in range(len(simulations)):
     snapshot = ParticleReadConversion_EagleSnapshot(tag, simulations[i], model, relitive_data_root)
 
-    galaxy_picture_edge_offsets = snapshot.convert_distance_values(galaxy_picture_edge_offsets__global, UnitSystem.physical, UnitSystem.cgs)
-    galaxy_picture_edge_offsets2 = [10**23, 10**22, 10**22]# cgs units
-
-
-
-    
-    lower_limits = np.full((3, ), -galaxy_picture_edge_offsets[i]) + galaxy_centres[i]
-    upper_limits = np.full((3, ), galaxy_picture_edge_offsets[i]) + galaxy_centres[i]
-
     r_200 = load_catalouge_field("Group_R_Crit200", "FOF", tag, simulations[i], SimulationModels.RECAL, relitive_data_root, UnitSystem.cgs)[assembily_history[simulations[i]][tag]["halo"] - 1]
     #selection_radius = r_200
-    selection_radius = 9.257*(10**22)
-
-
-
-
+    selection_radius = 9.257*(10**22)# 30 KPc
 
     particle_types = (ParticleType.gas, ParticleType.star, ParticleType.black_hole)
     net_angular_momentum = np.array([0, 0, 0], dtype = np.float64)
     for j in range(len(particle_types)):
-
-
-
-
-
-        #particle_locations_box_adjusted = snapshot.centre_particles(snapshot.particle_read(particle_types[j], "Coordinates", lower_limits, upper_limits, UnitSystem.cgs, UnitSystem.cgs), galaxy_centres[i])
-        #particle_velocities = snapshot.particle_read(particle_types[j], "Velocity", lower_limits, upper_limits, UnitSystem.cgs, UnitSystem.cgs)
-        #particle_masses = snapshot.particle_read(particle_types[j], "Mass", lower_limits, upper_limits, UnitSystem.cgs, UnitSystem.cgs)
-
         _, particle_locations_box_adjusted = snapshot.particle_read_sphere(particle_types[j], "Coordinates", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
         particle_velocities = snapshot.particle_read_sphere(particle_types[j], "Velocity", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs)
+        particle_velocities[:, 0] -= particle_velocities[:, 0].mean()
+        particle_velocities[:, 1] -= particle_velocities[:, 1].mean()
+        particle_velocities[:, 2] -= particle_velocities[:, 2].mean()
         particle_masses = snapshot.particle_read_sphere(particle_types[j], "Mass", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs)
 
-        
-        
         net_angular_momentum += np.sum(np.cross(particle_locations_box_adjusted, particle_velocities) * particle_masses[:, None], axis = 0)
 
     rotation_axis_vector = net_angular_momentum / np.linalg.norm(net_angular_momentum, 2)
@@ -63,19 +39,8 @@ for i in range(len(simulations)):
     particle_types = (ParticleType.gas, ParticleType.star, ParticleType.dark_matter)
     particle_type_graph_names = ("Gas", "Star" ,"Dark Matter")
     for j in range(len(particle_types)):
-
-
-
-
-
-        #particle_locations_box_adjusted = snapshot.centre_particles(snapshot.particle_read(particle_types[j], "Coordinates", lower_limits, upper_limits, UnitSystem.cgs, UnitSystem.cgs), galaxy_centres[i])
-        
         particle_locations, particle_locations_box_adjusted = snapshot.particle_read_sphere(particle_types[j], "Coordinates", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
         particle_locations_box_adjusted /= constants.SimulationConstants.get_constants()["CM_PER_MPC"]
-
-
-
-
 
         particle_locations_transformed, _ = transform_coordinates(particle_locations_box_adjusted, rotation_plane_x_vector, rotation_axis_vector)
 
