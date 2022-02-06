de6500d188af9c110f2b4cb83eb60a7ba1f5243d
Christopher Rowe <chris.rowe19@outlook.com>
2022-02-06 15:50:39 +0000

    Adds scripts for graphing angular momentum.

---
+++ angular_momentum_breakdown.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+from DataAccess import constants, load_catalouge_field, ParticleReadConversion_EagleSnapshot, particles_by_group, Simulations, SimulationModels, ParticleType, UnitSystem
+
+from assembily_history import physical_centeral_mass_positions, assembily_history
+from data_over_time import produce_simulations_graph, produce_single_simulation_graphs
+
+relitive_data_root = ".\\gm_for_mphys"
+
+def set_particle_type(func, particle: ParticleType):
+    def wrapper(*args, **kwargs):
+        return func(particle, *args, **kwargs)
+    return wrapper
+
+def angular_momenta_data(halo, subhalo, tag, simulation):
+    galaxy_centre = physical_centeral_mass_positions[simulation][tag]#TODO: check whether cgs or physical code is uncommented!!!
+    if galaxy_centre is None:
+        raise LookupError("No data avalible.")
+
+    r_200 = load_catalouge_field("Group_R_Crit200", "FOF", tag, simulation, SimulationModels.RECAL, relitive_data_root, UnitSystem.cgs)[assembily_history[simulation][tag]["halo"] - 1]
+    #selection_radius = r_200
+    selection_radius = 9.257*(10**22)# 30 KPc
+
+    snapshot = ParticleReadConversion_EagleSnapshot(tag, simulation, SimulationModels.RECAL, relitive_data_root)
+
+    # Calculate the net angular momentum (axis vector)
+    particle_types = (ParticleType.gas, ParticleType.star, ParticleType.black_hole)
+    net_angular_momentum = np.array([0, 0, 0], dtype = np.float64)
+    angular_momenta = {particle_type:np.array([0, 0, 0], dtype = np.float64) for particle_type in particle_types}
+    for particle_type in particle_types:
+        _, particle_locations_box_adjusted = snapshot.particle_read_sphere(particle_type, "Coordinates", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
+        particle_velocities = snapshot.particle_read_sphere(particle_type, "Velocity", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs)
+        particle_velocities[:, 0] -= particle_velocities[:, 0].mean()
+        particle_velocities[:, 1] -= particle_velocities[:, 1].mean()
+        particle_velocities[:, 2] -= particle_velocities[:, 2].mean()
+        particle_masses = snapshot.particle_read_sphere(particle_type, "Mass", galaxy_centre, selection_radius, UnitSystem.cgs, UnitSystem.cgs)
+
+        angular_momenta[particle_type] = np.sum(np.cross(particle_locations_box_adjusted, particle_velocities) * particle_masses[:, None], axis = 0)
+        net_angular_momentum += angular_momenta[particle_type]
+
+    return angular_momenta, net_angular_momentum
+
+def angular_momentum_fraction(target_particle_type, halo, subhalo, tag, simulation):
+    angular_momenta, net_angular_momentum = angular_momenta_data(halo, subhalo, tag, simulation)
+        
+    total_angular_momentum_length = np.linalg.norm(net_angular_momentum, 2)
+
+    if isinstance(target_particle_type, (list, tuple, np.ndarray)):
+        target_angular_momentum = np.array([0, 0, 0], dtype = np.float64)
+        for particle_type in target_particle_type:
+            target_angular_momentum += angular_momenta[particle_type]
+
+    else:
+        target_angular_momentum = angular_momenta[target_particle_type]
+
+    return net_angular_momentum.dot(target_angular_momentum) / (total_angular_momentum_length**2)
+
+def gas_star_angular_momentum_angle(halo, subhalo, tag, simulation):
+    angular_momenta, _ = angular_momenta_data(halo, subhalo, tag, simulation)
+
+    return np.arccos(angular_momenta[ParticleType.gas].dot(angular_momenta[ParticleType.star]) / (np.linalg.norm(angular_momenta[ParticleType.gas], 2) * np.linalg.norm(angular_momenta[ParticleType.star], 2))) * 180 / np.pi
+
+
+
+produce_simulations_graph(set_particle_type(angular_momentum_fraction, ParticleType.gas), "Fraction of Total Galactic $\\vec{L}$", "Gaseous $\\vec{L}$ Fraction", log_x = True)
+produce_simulations_graph(set_particle_type(angular_momentum_fraction, ParticleType.star), "Fraction of Total Galactic $\\vec{L}$", "Stellar $\\vec{L}$ Fraction", log_x = True)
+produce_single_simulation_graphs([set_particle_type(angular_momentum_fraction, ParticleType.gas), set_particle_type(angular_momentum_fraction, ParticleType.star)], ["Gas", "Star"], "Fraction of Total Galactic $\\vec{L}$", "$\\vec{L}$ Fraction", log_x = True)
+produce_simulations_graph(gas_star_angular_momentum_angle, "$\\theta$ (Degrees)", "Gaseous-Stellar $\\vec{L}$ Angular Seperation", log_x = True)
--- data_over_time.py
+++ data_over_time.py
@@ -74,8 +74,56 @@ def produce_simulations_graph(func, y_axis_label, graph_title_partial, log_x = F
     plt.legend()
     plt.show()
 
+#def produce_single_simulation_graphs(funcs, quantity_labels, y_axis_label, graph_title_partial, log_x = False, log_y = False):
+#    for simulation in (Simulations.Early, Simulations.Organic, Simulations.Late):
+#        quantity_values = []
+#        snapshot_numbers = []
+#        for i in range(len(funcs)):
+#            quantity_values.append(np.empty(shape = (len(constants.tags), )))
+#            snapshot_numbers.append(np.array([float("{}.{}".format(tag[5:8], tag[9:12])) for tag in constants.tags]))
+        
+#        for j, tag in enumerate(constants.tags):
+#            try:
+#                halo = assembily_history[simulation][tag]["halo"]
+#                subhalo = assembily_history[simulation][tag]["subhalo"]
+#                if halo is None:
+#                    raise LookupError("No history avalible.")
+
+#                for i, func in enumerate(funcs):
+#                    try:
+#                        quantity_values[i][j] = func(halo, subhalo, tag, simulation)
+#                    except LookupError:
+#                        quantity_values[i][j] = None
+                
+#            except LookupError:
+#                for i in range(len(funcs)):
+#                    quantity_values[i][j] = None
+            
+#        for i in range(len(funcs)):
+#            array_filter = True ^ np.isnan(quantity_values[i])
+#            quantity_values[i] = quantity_values[i][array_filter]
+#            snapshot_numbers[i] = snapshot_numbers[i][array_filter]
+        
+#            plt.plot(snapshot_numbers[i], quantity_values[i], label = quantity_labels[i])
+
+#        if log_x and log_y:
+#            plt.loglog()
+#        elif log_x:
+#            plt.semilogx()
+#        elif log_y:
+#            plt.semilogy()
+#        plt.xlim(plt.xlim()[1], plt.xlim()[0])
+#        plt.xlabel("Redshift")
+#        plt.ylabel(y_axis_label)
+#        simulation_name = Simulations.to_string(simulation)
+#        plt.title(f"{simulation_name} {graph_title_partial} for each Snapshot")
+#        plt.legend()
+#        plt.show()
+
 def produce_single_simulation_graphs(funcs, quantity_labels, y_axis_label, graph_title_partial, log_x = False, log_y = False):
-    for simulation in (Simulations.Early, Simulations.Organic, Simulations.Late):
+    fig, axes = plt.subplots(1, 3)
+
+    for simulation_number, simulation in enumerate((Simulations.Early, Simulations.Organic, Simulations.Late)):
         quantity_values = []
         snapshot_numbers = []
         for i in range(len(funcs)):
@@ -104,33 +152,35 @@ def produce_single_simulation_graphs(funcs, quantity_labels, y_axis_label, graph
             quantity_values[i] = quantity_values[i][array_filter]
             snapshot_numbers[i] = snapshot_numbers[i][array_filter]
         
-            plt.plot(snapshot_numbers[i], quantity_values[i], label = quantity_labels[i])
+            axes[simulation_number].plot(snapshot_numbers[i], quantity_values[i], label = quantity_labels[i])
 
         if log_x and log_y:
-            plt.loglog()
+            axes[simulation_number].loglog()
         elif log_x:
-            plt.semilogx()
+            axes[simulation_number].semilogx()
         elif log_y:
-            plt.semilogy()
-        plt.xlim(plt.xlim()[1], plt.xlim()[0])
-        plt.xlabel("Redshift")
-        plt.ylabel(y_axis_label)
+            axes[simulation_number].semilogy()
+        axes[simulation_number].set_xlim(axes[simulation_number].get_xlim()[1], axes[simulation_number].get_xlim()[0])
+        axes[simulation_number].set_xlabel("Redshift")
+        axes[simulation_number].set_ylabel(y_axis_label)
         simulation_name = Simulations.to_string(simulation)
-        plt.title(f"{simulation_name} {graph_title_partial} for each Snapshot")
-        plt.legend()
-        plt.show()
+        #axes[simulation_number].title(f"{simulation_name} {graph_title_partial} for each Snapshot")
+        axes[simulation_number].set_title(f"{simulation_name}")
+        axes[simulation_number].legend()
+    plt.suptitle(f"{graph_title_partial}")
+    plt.show()
 
 
 
 if __name__ == "__main__":
-    produce_simulations_graph(total_mass_M200, y_axis_label = "$M_{200}$ ($M_{sun}$)", graph_title_partial = "Group_M_Crit200", log_x = True, log_y = True)
-    produce_simulations_graph(stellar_mass, y_axis_label = "M* ($M_{sun}$)", graph_title_partial = "Central Subhalo Stellar Mass", log_x = True, log_y = True)
-    produce_simulations_graph(black_hole_mass, y_axis_label = "$M_{BH}$ ($M_{sun}$)", graph_title_partial = "Central Subhalo Black Hole Mass", log_x = True, log_y = True)
-    #produce_simulations_graph(dark_matter_mass, y_axis_label = "$M_{DM}$ ($M_{sun}$)", graph_title_partial = "Central Subhalo Dark Matter Mass", log_x = True, log_y = True)
-    #produce_simulations_graph(baryon_mass, y_axis_label = "$M_{Baryonic}$ ($M_{sun}$)", graph_title_partial = "Central Subhalo Baryonic Mass", log_x = True, log_y = True)
-    #t = -7#8
-    #print(assembily_history[Simulations.Early][constants.tags[t]])
-    #a = gas_mass(assembily_history[Simulations.Early][constants.tags[t]]["halo"], assembily_history[Simulations.Early][constants.tags[t]]["subhalo"], constants.tags[t], Simulations.Early)
+    #produce_simulations_graph(total_mass_M200, y_axis_label = "$M_{200}$ ($M_{sun}$)", graph_title_partial = "Group_M_Crit200", log_x = True, log_y = True)
+    #produce_simulations_graph(stellar_mass, y_axis_label = "M* ($M_{sun}$)", graph_title_partial = "Central Subhalo Stellar Mass", log_x = True, log_y = True)
+    #produce_simulations_graph(black_hole_mass, y_axis_label = "$M_{BH}$ ($M_{sun}$)", graph_title_partial = "Central Subhalo Black Hole Mass", log_x = True, log_y = True)
+    ##produce_simulations_graph(dark_matter_mass, y_axis_label = "$M_{DM}$ ($M_{sun}$)", graph_title_partial = "Central Subhalo Dark Matter Mass", log_x = True, log_y = True)
+    ##produce_simulations_graph(baryon_mass, y_axis_label = "$M_{Baryonic}$ ($M_{sun}$)", graph_title_partial = "Central Subhalo Baryonic Mass", log_x = True, log_y = True)
+    ##t = -7#8
+    ##print(assembily_history[Simulations.Early][constants.tags[t]])
+    ##a = gas_mass(assembily_history[Simulations.Early][constants.tags[t]]["halo"], assembily_history[Simulations.Early][constants.tags[t]]["subhalo"], constants.tags[t], Simulations.Early)
     produce_single_simulation_graphs([dark_matter_mass, gas_mass, stellar_mass, black_hole_mass], ["Dark Matter", "Gas", "Stars", "Black Holes"], y_axis_label = "Mass ($M_{sun}$)", graph_title_partial = "Central Subhalo Mass Brakedown (r = 30 kPc)", log_x = True, log_y = True)
 
     #produce_single_simulation_graphs([total_mass_M200, dark_matter_mass, baryon_mass, lambda *args, **kwargs: dark_matter_mass(*args, **kwargs) + baryon_mass(*args, **kwargs)], ["Halo $M_{200}$", "Dark Matter (r = 30 kPc)", "Baryonic Matter (r = 30 kPc)", "All Matter (r = 30 kPc)"], y_axis_label = "Mass ($M_{sun}$)", graph_title_partial = "Central Subhalo Mass", log_x = True, log_y = True)
