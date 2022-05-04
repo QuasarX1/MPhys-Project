593533cdacc918725afd026ff24a470b4751f0ec
Christopher Rowe <chris.rowe19@outlook.com>
2022-05-05 00:06:14 +0100

    Conficures image generation to use CUDA.

--- poster_background_images.py
+++ poster_background_images.py
@@ -3,6 +3,10 @@ from scipy.stats import norm
 from matplotlib import pyplot as plt
 from PIL import Image
 
+import sys
+sys.path.append("C:\\Scripts\\")
+from cuda_factory import CUDA_Kernel
+
 from DataAccess import ParticleReadConversion_EagleSnapshot, load_catalouge_field, Simulations, SimulationModels, ParticleType, UnitSystem, constants
 from Physics import angular_momentum
 
@@ -18,7 +22,7 @@ galaxy_centres = [physical_centeral_mass_positions[sim][constants.tags[-1]] for 
 
 simulations = (Simulations.Early, Simulations.Organic, Simulations.Late)
 for i in range(len(simulations)):
-    if i == 0: continue
+    #if i == 0: continue
 
     snapshot = ParticleReadConversion_EagleSnapshot(tag, simulations[i], model, relitive_data_root)
 
@@ -31,39 +35,13 @@ for i in range(len(simulations)):
     for j in range(len(particle_types)):
         _, particle_locations_box_adjusted = snapshot.particle_read_sphere(particle_types[j], "Coordinates", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
         particle_velocities = snapshot.particle_read_sphere(particle_types[j], "Velocity", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs)
-        #particle_velocities -= particle_velocities.mean(axis = 0)
         particle_masses = snapshot.particle_read_sphere(particle_types[j], "Mass", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs)
-
-        #net_angular_momentum += np.sum(np.cross(particle_locations_box_adjusted, particle_velocities) * particle_masses[:, None], axis = 0)
+        
         net_angular_momentum += angular_momentum(particle_locations_box_adjusted, particle_velocities, particle_masses)
 
     rotation_axis_vector = net_angular_momentum / np.linalg.norm(net_angular_momentum, 2)
     rotation_plane_x_vector = produce_disk_x_basis(rotation_axis_vector)
 
-    #particle_types = (ParticleType.gas, ParticleType.star)
-    #particle_type_graph_names = ("Gas", "Star")
-    #for j in range(len(particle_types)):
-    #    particle_locations, particle_locations_box_adjusted = snapshot.particle_read_sphere(particle_types[j], "Coordinates", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
-    #    particle_locations_box_adjusted /= constants.SimulationConstants.get_constants()["CM_PER_MPC"] * 10**-3
-
-    #    particle_locations_transformed, _ = transform_coordinates(particle_locations_box_adjusted, rotation_plane_x_vector, rotation_axis_vector)
-
-    #    fig = plt.figure(figsize = (24, 24))
-
-    #    #ax = plt.subplot(2, 2, 3, aspect = "equal")
-    #    ax = plt.gca()
-    #    ax.set_facecolor("black")
-    #    ax.set_xlabel("X (kpc)")
-    #    ax.set_ylabel("Z (kpc)")
-    #    ax.scatter(particle_locations_transformed[:, 0], particle_locations_transformed[:, 2], s = 0.1, c = "y")
-    #    x_lims = ax.get_xlim()
-    #    y_lims = ax.get_ylim()
-    #    axis_lims = (min(x_lims[0], y_lims[0]), max(x_lims[1], y_lims[1]))
-    #    ax.set_xlim(*axis_lims)
-    #    ax.set_ylim(*axis_lims)
-    
-    #    fig.suptitle("{} Particle Projection of the Assembled Galaxy in the {} Regime".format(particle_type_graph_names[j], Simulations.to_string(simulations[i]).title()))
-    #    plt.show()
 
 
     _, gas_particle_locations_box_adjusted = snapshot.particle_read_sphere(ParticleType.gas, "Coordinates", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
@@ -72,66 +50,97 @@ for i in range(len(simulations)):
     gas_density = snapshot.particle_read_sphere(ParticleType.gas, "Density", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs)
     gas_smoothing_length = snapshot.particle_read_sphere(ParticleType.gas, "SmoothingLength", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs)
     gas_smoothing_length /= constants.SimulationConstants.get_constants()["CM_PER_MPC"] * 10**-3
-    #gas_smoothing_length *= 0.1
 
     _, star_particle_locations_box_adjusted = snapshot.particle_read_sphere(ParticleType.star, "Coordinates", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs, return_coordinates = True)
     star_particle_locations_box_adjusted /= constants.SimulationConstants.get_constants()["CM_PER_MPC"] * 10**-3
     star_particle_locations_transformed, _ = transform_coordinates(star_particle_locations_box_adjusted, rotation_plane_x_vector, rotation_axis_vector)
     star_smoothing_length = snapshot.particle_read_sphere(ParticleType.star, "SmoothingLength", galaxy_centres[i], selection_radius, UnitSystem.cgs, UnitSystem.cgs)
     star_smoothing_length /= constants.SimulationConstants.get_constants()["CM_PER_MPC"] * 10**-3
-    #star_smoothing_length *= 0.1
 
-    #print(max(np.sqrt(np.sum(gas_particle_locations_transformed**2, axis = 1))))
-    #print(max(gas_smoothing_length))
-    #print()
-    #print(max(np.sqrt(np.sum(star_particle_locations_transformed**2, axis = 1))))
-    #print(max(star_smoothing_length))
-    #print()
-    #print()
 
-    gas_xy = gas_particle_locations_transformed[:, 0:3:2]
-    star_xy = star_particle_locations_transformed[:, 0:3:2]
 
-    #aspect_ratio = 1
-    aspect_ratio = 41 / 58
+    #gas_xy = gas_particle_locations_transformed[:, 0:3:2]
+    #star_xy = star_particle_locations_transformed[:, 0:3:2]
+    gas_xy = gas_particle_locations_transformed[:, 1:3]
+    star_xy = star_particle_locations_transformed[:, 1:3]
+
+    aspect_ratio = 1
+    #aspect_ratio = 41 / 58
 
     width = 2000
     height = int(aspect_ratio * width)
-    pixels = np.zeros(shape = (width, height), dtype = float)
+    
+    c_func = """\
+    extern "C" __global__
+    void make_image(float *pixels, int width, float aspect_ratio, float physical_radius, int n_gas, int n_stars, float *gas_x, float *gas_y, float *star_x, float *star_y, float *gas_smoothing_length, float *star_smoothing_length)
+    {
+        int i;
+        
+        size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for x in range(width):
-        #x_physical = (x - (width / 2)) * selection_radius / (constants.SimulationConstants.get_constants()["CM_PER_MPC"] * 10**-3)
-        x_physical = ((x / width) - 0.5) * 30
-        for y in range(height):
-            #y_physical = (y - (height / 2)) * selection_radius / (constants.SimulationConstants.get_constants()["CM_PER_MPC"] * 10**-3)
-            y_physical = ((y / height) - 0.5) * 30 * aspect_ratio
-            
-            gas_intersection_filter = np.sqrt(np.sum((np.array([x_physical, y_physical]) - gas_xy)**2, axis = 1)) < gas_smoothing_length
-            #gas_value = sum(gas_smoothing_length[gas_intersection_filter] - np.sqrt(np.sum((np.array([x_physical, y_physical]) - gas_xy[gas_intersection_filter])**2, axis = 1)))
-            #gas_value = sum(np.sqrt(np.sum((np.array([x_physical, y_physical]) - gas_xy[gas_intersection_filter])**2, axis = 1)) / gas_smoothing_length[gas_intersection_filter])
-            gas_value = sum(norm.pdf(np.sqrt(np.sum((np.array([x_physical, y_physical]) - gas_xy[gas_intersection_filter])**2, axis = 1)) / gas_smoothing_length[gas_intersection_filter], loc = 0, scale = 0.1))
+        if (tid == 0) { printf("%d, %f %d, %d\\n", width, aspect_ratio, n_gas, n_stars); }
 
-            star_intersection_filter = np.sqrt(np.sum((np.array([x_physical, y_physical]) - star_xy)**2, axis = 1)) < star_smoothing_length
-            #star_value = sum(star_smoothing_length[star_intersection_filter] - np.sqrt(np.sum((np.array([x_physical, y_physical]) - star_xy[star_intersection_filter])**2, axis = 1)))
-            #star_value = sum(np.sqrt(np.sum((np.array([x_physical, y_physical]) - star_xy[star_intersection_filter])**2, axis = 1)) / star_smoothing_length[star_intersection_filter])
-            star_value = sum(norm.pdf(np.sqrt(np.sum((np.array([x_physical, y_physical]) - star_xy[star_intersection_filter])**2, axis = 1)) / star_smoothing_length[star_intersection_filter], loc = 0, scale = 0.1))
-            
-            pixels[x, y] = star_value - gas_value
-            if pixels[x, y] < 0:
-                pixels[x, y] = 0
-            
-            if gas_value > 0 or star_value > 0: print("({}, {}) at ({:.2f}, {:.2f}):    {:.4f} - {:.4f}".format(x, y, x_physical, y_physical, star_value, gas_value))
-                
-    pixels *= 255 / pixels.max() * 0.7
-    pixels = np.array(pixels, dtype = int)
+        int height = (int)(aspect_ratio * width);
 
-    image = Image.fromarray(pixels.transpose()).convert("RGB")
-    img_pixels = image.load()
-    for pixel_x in range(image.size[0]):
-        for pixel_y in range(image.size[1]):
-            img_pixels[pixel_x, pixel_y] = (img_pixels[pixel_x, pixel_y][0], int(img_pixels[pixel_x, pixel_y][1] * 0.7), 0)
-    image.save("{} rendered image.png".format(Simulations.to_string(simulations[i])))
+        if (tid < width * height)
+        {
+            int x = tid % width;
+            float x_physical = (((float)x / (float)width) - 0.5) * physical_radius;
 
-    #print(pixels)
-    #plt.imshow(pixels, cmap = "gray", vmin = 0, vmax = pixels.max())
-    #plt.show()
+            int y = (int)(tid / width);
+            float y_physical = (((float)y / (float)height) - 0.5) * physical_radius * aspect_ratio;
+
+            // Gas
+            float gas_value = 0.0;
+            for (i = 0; i < n_gas; ++i)
+            {
+                float dist_to_particle = sqrtf(powf(x_physical - gas_x[i], 2.0) + powf(y_physical - gas_y[i], 2.0));
+                if (dist_to_particle < gas_smoothing_length[i])
+                {
+                    float norm_x = dist_to_particle / gas_smoothing_length[i], norm_mu = 0.0, norm_sigma = 0.06, norm_a = 2.5;
+                    //gas_value += 1.0;
+                    gas_value += norm_a * powf(2.718281828459, -0.5 * powf((norm_x - norm_mu) / norm_sigma, 2.0)) / (norm_sigma * sqrtf(2.0 * 3.1415926535));
+                }
+            }
+
+            // Star
+            float star_value = 0.0;
+            for (i = 0; i < n_stars; ++i)
+            {
+                float dist_to_particle = sqrtf(powf(x_physical - star_x[i], 2.0) + powf(y_physical - star_y[i], 2.0));
+                if (dist_to_particle < star_smoothing_length[i])
+                {
+                    float norm_x = dist_to_particle / star_smoothing_length[i], norm_mu = 0.0, norm_sigma = 0.01, norm_a = 1.0;
+                    //star_value += 1.0;
+                    star_value += norm_a * powf(2.718281828459, -0.5 * powf((norm_x - norm_mu) / norm_sigma, 2.0)) / (norm_sigma * sqrtf(2.0 * 3.1415926535));
+                }
+            }
+
+            pixels[tid] = star_value - gas_value;
+            //pixels[tid] = star_value;
+            //pixels[tid] = gas_value;
+            if (pixels[tid] < 0.0) { pixels[tid] = 0.0; }
+            printf("%f %f %f\\n", pixels[tid], star_value, gas_value);
+        }
+    }
+    """
+    n_pixels = width * height
+    r = selection_radius / (constants.SimulationConstants.get_constants()["CM_PER_MPC"] * 10**-3)
+    make_image = CUDA_Kernel(c_func, "make_image",
+                             [np.float32, np.int32, np.float32, np.float32, np.int32, np.int32, np.float32,  np.float32,  np.float32,   np.float32,   np.float32,  np.float32],
+                             [n_pixels,   1,        1,          1,          1,        1,        len(gas_xy), len(gas_xy), len(star_xy), len(star_xy), len(gas_xy), len(star_xy)],
+                             [0])
+    make_image.blocks = CUDA_Kernel.minimum_needed_blocks(n_pixels)
+    #make_image.threads_per_block = 512
+    pixels = make_image(width, aspect_ratio, r, len(gas_xy), len(star_xy), gas_xy[:, 0], gas_xy[:, 1], star_xy[:, 0], star_xy[:, 1], gas_smoothing_length, star_smoothing_length)[:n_pixels]
+    pixels = pixels.reshape((width, height))
+    
+    pixels = (pixels * 255 / pixels.max()).astype(int).transpose()
+    
+    pixels = pixels[:, :, np.newaxis]
+    rgb_pixels = np.append(pixels, 0.7 * pixels, axis = 2)
+    rgb_pixels = np.append(rgb_pixels, 0 * pixels, axis = 2)
+    pixels = rgb_pixels.astype("uint8")
+    image = Image.fromarray(pixels, "RGB")
+
+    image.save("rendered image {}.png".format(Simulations.to_string(simulations[i])))
